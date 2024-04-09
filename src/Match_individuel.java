9cc54590aec29aea8409715713f7bd25bf4e49e9
julian2bot <marquesjulian26@gmail.com>
2024-04-09 14:41:09 +0200

    edit Mathc individuel + pays constructeur + ajout init liste

--- src/Match_individuel.java
+++ src/Match_individuel.java
@@ -9,6 +9,9 @@ public class Match_individuel extends Match {
 	 * Default constructor
 	 */
 	public Match_individuel(int nbTour, String nomTour, Epreuve epreuve) {
+		this.nbTour = nbTour;
+		this.nomTour = nomTour;
+		this.epreuve = epreuve;
 	}
 
 	/**
--- src/Pays.java
+++ src/Pays.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class Pays {
 	private String nom;
+	List<Object> lesEquipes;
 
 	/**
 	 * @param nom
