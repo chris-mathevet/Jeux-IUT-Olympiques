76637b0b8ba7a8b0f1a427600b1361495e35df64
julian2bot <marquesjulian26@gmail.com>
2024-04-15 21:03:37 +0200

    edit les sports + tests ok

--- src/Natation.java
+++ src/Natation.java
@@ -34,7 +34,8 @@ public class Natation extends Sport {
 	public int bareme(Athlete athlete){
 		// agilité + (endurance*random(1 => 2) * force) ==> 0+(0*0*0) à 20+(20*2*20) 
 		Random e = new Random();
-		return (int)((athlete.getAgilite() * 2 + athlete.getForce() + (e.nextInt(20)+1) * 2 + athlete.getEndurance() * 5)/10);
+		// return (int)((athlete.getAgilite() * 2 + athlete.getForce() + (e.nextInt(20)+1) * 2 + athlete.getEndurance() * 5)/10);
+		return (int)(athlete.getAgilite() * 2 + athlete.getForce() + (e.nextInt(20)+1) * 2 + athlete.getEndurance() * 5);
 	}
 
 
