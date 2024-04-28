157c7c2f2aae56c701e2f3293d20aa3f8dd3f73d
Julian <160622256+julian2bot@users.noreply.github.com>
2024-04-28 23:49:27 +0200

    Update Testdatabase.java

--- src/database/Testdatabase.java
+++ src/database/Testdatabase.java
@@ -7,7 +7,7 @@ public class Testdatabase{
     public void testSport() {
         Select lesjoueurs = new Select();
 
-        assertTrue(lesjoueurs.getMdp() == "marques");
+        assertTrue(lesjoueurs.getMdp() == " ");
         assertTrue(lesjoueurs.getUrl() == "jdbc:mysql://localhost:3306/saejava");
         assertTrue(lesjoueurs.getUser() == "root");
 
