ca3b88afaa9d62874b669429344dd63d2ff6032b
Julian <160622256+julian2bot@users.noreply.github.com>
2024-04-28 14:36:07 +0200

    Update create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,4 +1,7 @@
 
+-- Tableaux
+
+
 CREATE TABLE PAYS (
     idPays INT PRIMARY KEY,
     nomPays VARCHAR(20)
@@ -72,71 +75,4 @@ CREATE TABLE PARTICIPER_EQUIPE (
     PRIMARY KEY (idMatch, idEquipe),
     FOREIGN KEY (idMatch) REFERENCES MATCH_TABLE (idMatch),
     FOREIGN KEY (idEquipe) REFERENCES EQUIPE (idEquipe)
-);
-=======
--- table
-create table PAYS(
-    idPays int primary key,
-    nomPays varchar(20)
-);
-create table SPORT(
-    idSport int,
-    nomSport varchar(20),
-    nbParEquipe int
-);
-create table ATHLETE(
-    idAthlete int primary key,
-    nomAthlete varchar(20),
-    prenomAthlete varchar(20),
-    sexe char,
-    force int,
-    endurance int,
-    agilite int,
-    idPays int, 
-    FOREIGN KEY (idPays) REFERENCES PAYS (idPays)
-);
-create table EPREUVE(
-    idEpreuve int primary key,
-    description varchar(250),
-    typeEpreuve varchar(20),
-    idSport int, 
-    FOREIGN KEY (idSport) REFERENCES SPORT (idSport)
-);
-create table MATCH(
-    idMatch int primary key,
-    typeMatch varchar(20),
-    idEpreuve int,
-    FOREIGN KEY(idEpreuve) REFERENCES EPREUVE (idEpreuve)
-);
-create table EQUIPE(
-    idEquipe int primary key,
-    nomEquipe varchar(20)
-);
-
--- asso
-
-create table EST_CONSTITUER(
-    idEquipe int,
-    idAthlete int,
-    primary key(idEquipe, idAthlete),
-    FOREIGN KEY (idEquipe) REFERENCES EQUIPE(idEquipe),
-    FOREIGN KEY (idAthlete) REFERENCES ATHLETE(idAthlete)
-);
-
-create table PARTICIPER_ATHLETE(
-    idMatch int,
-    idAthlete int,
-    resultat int,
-    primary key(idMatch, idAthlete),
-    FOREIGN KEY (idMatch) REFERENCES MATCH(idMatch),
-    FOREIGN KEY (idAthlete) REFERENCES ATHLETE(idAthlete)
-);
-
-create table PARTICIPER_EQUIPE(
-    idMatch int,
-    idEquipe int,
-    resultat int,
-    primary key(idMatch, idEquipe),
-    FOREIGN KEY (idMatch) REFERENCES MATCH(idMatch),
-    FOREIGN KEY (idEquipe) REFERENCES EQUIPE(idEquipe)
 );
