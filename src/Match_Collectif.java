252b52efcf013e08740956a1dc0ab15e35e82eca
TronnoxUwU <baptisterichard98@free.fr>
2024-05-07 15:10:30 +0200

    fonction de Match

--- src/Match_Collectif.java
+++ src/Match_Collectif.java
@@ -4,19 +4,36 @@ import java.util.List;
  * 
  */
 public class Match_Collectif extends Match {
-
+	private Epreuve_Collective epreuve;
 	/**
 	 * Default constructor
 	 */
-	public Match_Collectif(int nbTour, String nomTour, Epreuve epreuve) {
+	public Match_Collectif(int nbTour, String nomTour, Epreuve_Collective epreuve) {
+		super(nbTour, nomTour);
+		this.epreuve = epreuve;
+		this.epreuve.ajoutMatch(this);
 	}
-
-	/**
-	 * @return
-	 */
-	public List<Equipe> resultat() {
-		// TODO implement here
-		return null;
+	public Epreuve_Collective getEpreuve() {
+		return epreuve;
 	}
-
+	public int getResultatEquipe(Equipe equipe){
+		int index = this.epreuve.getLesParticipants().indexOf(equipe);
+		if(index == -1){
+			return -1;
+		}
+		else{
+			return super.getResultats().get(index);
+		}	
+	}
+	public List<Integer> resultat() {
+		if(super.getResultats().isEmpty()){
+			List<Integer> res = super.getResultats();
+			List<Equipe> participants = this.epreuve.getLesParticipants();
+			for(Equipe equipe : participants){
+				res.add(equipe.participer(this));
+			}
+			return res;
+		}
+		return super.getResultats();
+	}
 }
