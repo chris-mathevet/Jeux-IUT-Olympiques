e764e17ed62b89a5401365f5dbbc5fccdfb5bb16
julian2bot <marquesjulian26@gmail.com>
2024-05-08 20:45:01 +0200

    Equipe fait

--- src/Equipe.java
+++ src/Equipe.java
@@ -1,37 +1,57 @@
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Equipe implements Participant {
-	private String nom;
-	private List <Athlete> lesAthletes;
+    private String nom;
+    private List <Athlete> lesAthletes;
 
 
-	/**
-	 * @param nom
-	 */
-	public Equipe(String nom) {
-		this.nom = nom;
-		this.lesAthletes = new ArrayList<>();
-	}
+    /**
+     * @param nom
+     */
+    public Equipe(String nom) {
+        this.nom = nom;
+        this.lesAthletes = new ArrayList<>();
+    }
 
-	/**
-	 * @return
-	 */
-	public String getNom() {
-		return this.nom;
-	}
+    /**
+     * @return String 
+     * Nom de l'équipe
+     * 
+     */
+    public String getNom() {
+        return this.nom;
+    }
 
-	/**
-	 * @param Match match 
-	 * @return
-	 */
-	public int participer(Match match) {
-		int sommeBareme = 0;
-		for (Athlete athlete : this.lesAthletes) {
-			sommeBareme += match.getEpreuve().getSport().bareme(athlete);
-		}
+    /**
+     * Permet d'ajouter un athlete dans l'équipe, si il n'est pas déjà dedans sinon on renvoie l'exception "AlreadyInException" créer à cet effet.
+     * 
+     * @param Athlete athlete 
+     * @return void
+     */
 
-		return sommeBareme;
-	}
+    public void ajouter(Athlete athlete) throws AlreadyInException {
+        if (!(lesAthletes.contains(athlete))) {
+            lesAthletes.add(athlete);
+        }
+        else {
+            throw new AlreadyInException();
+        }
+    }
+
+    /**
+     * 
+     * Permet d'obtenir la somme des barèmes des athletes présents dans l'équipe.
+     * 
+     * @param Match match 
+     * @return int 
+     */
+    public int participer(Match match) {
+        int sommeBareme = 0;
+        for (Athlete athlete : this.lesAthletes) {
+            sommeBareme += match.getEpreuve().getSport().bareme(athlete);
+        }
+
+        return sommeBareme;
+    }
 }
