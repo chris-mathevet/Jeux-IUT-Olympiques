1c4fb9ea21ec3b68758fc059192d0ff46b81abfc
Axel Meunier <hacksel002@gmail.com>
2024-05-13 11:48:42 +0200

    Modification Equipe.java

--- src/Equipe.java
+++ src/Equipe.java
@@ -2,9 +2,9 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Equipe implements Participant {
+public class Equipe extends ArrayList<Athlete> implements Participant{
+
 	private String nom;
-	private List <Athlete> lesAthletes;
 
 
 	/**
@@ -12,7 +12,6 @@ public class Equipe implements Participant {
 	 */
 	public Equipe(String nom) {
 		this.nom = nom;
-		this.lesAthletes = new ArrayList<>();
 	}
 
 	/**
@@ -23,12 +22,46 @@ public class Equipe implements Participant {
 	}
 
 	/**
-	 * @param Match match 
+	 * 
 	 * @return
+	 */
+	public List<Athlete> getListeAthlete() {
+		return this;
+	}
+
+	/**
+     * Permet d'ajouter un athlete dans l'équipe, si il n'est pas déjà dedans sinon on renvoie l'exception "AlreadyInException" créer à cet effet.
+     * 
+     * @param Athlete athlete 
+     * @return void
+     */
+
+	 public void ajouter(Athlete athlete) throws AlreadyInException {
+
+		if (this.isEmpty()) {
+			this.add(athlete);
+		}
+
+
+        else if (!(this.contains(athlete)) && athlete.getPays().equals(this.get(0).getPays())) {
+            this.add(athlete);
+        }
+        else if (this.contains(athlete)) {
+            throw new AlreadyInException();
+        }
+
+		else {
+			System.err.println("Pas le même pays");
+		}
+    }
+
+	/**
+	 * @param Match match 
+	 * @return int (somme des bareme des athlete de l'équipe)
 	 */
 	public int participer(Match match) {
 		int sommeBareme = 0;
-		for (Athlete athlete : this.lesAthletes) {
+		for (Athlete athlete : this) {
 			sommeBareme += match.getEpreuve().getSport().bareme(athlete);
 		}
 
--- src/TestEquipe.java
+++ src/TestEquipe.java
@@ -7,8 +7,12 @@ public class TestEquipe {
     
     @Test
     public void test() {
+        Athlete a1 = new Athlete("george", "martin", "m", 9, 8, 5);
+        Athlete a2 = new Athlete("raphael", "nadal", "m", 9, 8, 5);
+        Athlete a3 = new Athlete("sophie", "duke", "f", 9, 8, 5);
         Equipe testEquipe = new Equipe("farfadetsMalicieux");
-        testEquipe.ajouter(new Athlete("Marin", "farf", 'M', 5, 5, 5));
-        assertEquals(testEquipe, testEquipe);
+        testEquipe.ajouter(a1);
+      
+
     }
 }
