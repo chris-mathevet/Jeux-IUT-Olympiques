8bf08a1beadc682ea5a5cb5ca9d1ed71ba3965ab
TronnoxUwU <baptisterichard98@free.fr>
2024-05-14 12:57:24 +0200

    actualisation des tests Match et Epreuve

--- src/TestEpreuveMatch.java
+++ src/TestEpreuveMatch.java
@@ -8,12 +8,13 @@ public class TestEpreuveMatch {
     @Test
     public void testMatchAthlete(){
         Athletisme athletisme = new Athletisme();
+        Pays france = new Pays("France");
 
-        Athlete baptiste = new Athlete("RICHARD","Baptiste",'M', 11, 18, 16);
-        Athlete chris = new Athlete("MATEHEVET","Chris",'F', 13,17, 15);
-        Athlete julian = new Athlete("MARQUES","Julian",'M', 15,10, 19);
-        Athlete axel = new Athlete("MEUNIER","Axel",'M', 19,18, 9);
-        Athlete michelle = new Athlete("MICHMICH","Michelle",'F', 12,16, 14);
+        Athlete baptiste = new Athlete("RICHARD","Baptiste",'M', 11, 18, 16,france);
+        Athlete chris = new Athlete("MATEHEVET","Chris",'F', 13,17, 15,france);
+        Athlete julian = new Athlete("MARQUES","Julian",'M', 15,10, 19,france);
+        Athlete axel = new Athlete("MEUNIER","Axel",'M', 19,18, 9,france);
+        Athlete michelle = new Athlete("MICHMICH","Michelle",'F', 12,16, 14,france);
 
         Epreuve<Athlete> mettre110H = new Epreuve<>("110 m haies hommes", athletisme, 'M');
         Epreuve<Athlete> mettre110F = new Epreuve<>("110 m haies femmes", athletisme, 'F');
@@ -44,35 +45,37 @@ public class TestEpreuveMatch {
         }
     }
     @Test
-    public void testMatchEquipe(){
-        Athlete baptiste = new Athlete("RICHARD","Baptiste",'M', 11, 18, 16);
-        Athlete chris = new Athlete("MATEHEVET","Chris",'M', 13,17, 15);
-        Athlete julian = new Athlete("MARQUES","Julian",'M', 15,10, 19);
-        Athlete axel = new Athlete("MEUNIER","Axel",'M', 19,18, 9);
-        Athlete shanka = new Athlete("ClERMONT","shanka",'M', 19,18, 9);
-        Athlete bastien = new Athlete("MONET","bastien",'M', 19,18, 9);
+    public void testMatchEquipe() throws AlreadyInException{
+        Pays france = new Pays("France");
+        
+        Athlete baptiste = new Athlete("RICHARD","Baptiste",'M', 11, 18, 16,france);
+        Athlete chris = new Athlete("MATEHEVET","Chris",'M', 13,17, 15,france);
+        Athlete julian = new Athlete("MARQUES","Julian",'M', 15,10, 19,france);
+        Athlete axel = new Athlete("MEUNIER","Axel",'M', 19,18, 9,france);
+        Athlete shanka = new Athlete("ClERMONT","shanka",'M', 19,18, 9,france);
+        Athlete bastien = new Athlete("MONET","bastien",'M', 19,18, 9,france);
 
-        Athlete riri = new Athlete("Duck","riri",'M', 1,8, 19);
-        Athlete fifi = new Athlete("Duck","fifi",'M', 12,5, 9);
-        Athlete loulou = new Athlete("Duck","loulou",'M', 19,8, 9);
-        Athlete pixou = new Athlete("Duck","pixou",'M', 19,12, 9);
-        Athlete donald = new Athlete("Duck","donald",'M', 19,18, 19);
-        Athlete flagada = new Athlete("Jones","Flagada",'M', 19,18, 19);
+        Athlete riri = new Athlete("Duck","riri",'M', 1,8, 19,france);
+        Athlete fifi = new Athlete("Duck","fifi",'M', 12,5, 9,france);
+        Athlete loulou = new Athlete("Duck","loulou",'M', 19,8, 9,france);
+        Athlete pixou = new Athlete("Duck","pixou",'M', 19,12, 9,france);
+        Athlete donald = new Athlete("Duck","donald",'M', 19,18, 19,france);
+        Athlete flagada = new Athlete("Jones","Flagada",'M', 19,18, 19,france);
 
         Equipe equipe1 = new Equipe("1");
-        equipe1.add(baptiste);
-        equipe1.add(chris);
-        equipe1.add(julian);
-        equipe1.add(axel);
-        equipe1.add(shanka);
-        equipe1.add(bastien);
+        equipe1.ajouter(baptiste);
+        equipe1.ajouter(chris);
+        equipe1.ajouter(julian);
+        equipe1.ajouter(axel);
+        equipe1.ajouter(shanka);
+        equipe1.ajouter(bastien);
         Equipe equipe2 = new Equipe("2");
-        equipe2.add(riri);
-        equipe2.add(fifi);
-        equipe2.add(loulou);
-        equipe2.add(pixou);   
-        equipe2.add(donald);
-        equipe2.add(flagada);
+        equipe2.ajouter(riri);
+        equipe2.ajouter(fifi);
+        equipe2.ajouter(loulou);
+        equipe2.ajouter(pixou);   
+        equipe2.ajouter(donald);
+        equipe2.ajouter(flagada);
 
         VoleyBall voley = new VoleyBall();
 
