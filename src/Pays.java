37e0aab2ef2cf84d7fd506524a38f6df682db7bd
Slyjack <62718253+slyjack999@users.noreply.github.com>
2024-05-15 22:07:54 +0200

    Ajout des médailles

--- src/Pays.java
+++ src/Pays.java
@@ -5,6 +5,9 @@ import java.util.List;
 public class Pays {
 	private String nom;
 	List<Object> lesEquipes;
+	private int medailleOr;
+	private int medailleArgent;
+	private int medailleBronze;
 
 	/**
 	 * @param String le nom du pays
@@ -12,13 +15,36 @@ public class Pays {
 	public Pays(String nom) {
 		this.nom = nom;
 		this.lesEquipes = new ArrayList<>();
+		this.medailleOr = 0;
+		this.medailleArgent = 0;
+		this.medailleBronze = 0;
 	}
 
-	/**
-	 * @return String le nom du pays
-	 */
 	public String getNomPays() {
 		return this.nom;
 	}
 
+	public int getMedailleOr() {
+		return this.medailleOr;
+	}
+
+	public int getMedailleArgent() {
+		return this.medailleArgent;
+	}
+
+	public int getMedailleBronze() {
+		return this.medailleBronze;
+	}
+
+	public void setMedailleOr(int medailleOr) {
+		this.medailleOr = medailleOr;
+	}
+
+	public void setMedailleArgent(int medailleArgent) {
+		this.medailleArgent = medailleArgent;
+	}
+
+	public void setMedailleBronze(int medailleBronze) {
+		this.medailleBronze = medailleBronze;
+	}
 }
