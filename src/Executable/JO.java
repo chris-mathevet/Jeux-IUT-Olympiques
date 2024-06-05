96c70960b4028bf329cfdd22c1e210d36a52e53b
Slyjack <62718253+slyjack999@users.noreply.github.com>
2024-06-05 14:32:40 +0200

    méthode pour créer un Pays

--- src/Executable/JO.java
+++ src/Executable/JO.java
@@ -41,8 +41,23 @@ public class JO {
         this.lesEquipes = new ArrayList<>();
         this.lesEpreuves = new ArrayList<>();
         this.lesPays = new ArrayList<>();
-        
         // Import de la BD
     }   
+
+    public void creerPays(){
+        System.out.println("Vous allez créer un pays\nEntrez un nom de Pays.");
+        String commandePays = System.console().readLine();
+        commandePays = commandePays.strip();
+        System.out.println("Voulez vous vraiment créer le pays : " + commandePays + " (Y/N)");
+        String commandeVerif = System.console().readLine();
+        commandeVerif = commandeVerif.strip().toUpperCase();
+        if(commandeVerif.equals("Y")){
+            this.lesPays.add(new Pays(commandePays));
+            System.out.println("Le pays " + commandePays + " a bien été rajouté");
+        }
+        else{
+            System.out.println("Le pays n'a pas été rajouté");
+        }
+    }
     
 }
