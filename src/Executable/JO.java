84901517fb802dc25b4947b1d82d7e5b89717b56
Slyjack <62718253+slyjack999@users.noreply.github.com>
2024-06-05 10:47:46 +0200

    enum Tris

--- src/Executable/JO.java
+++ src/Executable/JO.java
@@ -15,6 +15,23 @@ public class JO {
     private ArrayList<Epreuve<? extends Participant>> lesEpreuves;
     private ArrayList<Pays> lesPays;
 
+    private enum tris{NATUREL, GOLD, TOTAL}
+
+    private void triPays(tris leTri){
+        switch (leTri) {
+            case GOLD:
+                
+                break;
+            
+            case TOTAL:
+                
+                break;
+        
+            default:
+                break;
+        }
+    }    
+
     public void init(){
         this.lesSports = new ArrayList<>();
         this.lesAthletes = new ArrayList<>();
