b4140a0e46247555ea02033182acf53fdcf4296c
Slyjack <62718253+slyjack999@users.noreply.github.com>
2024-06-05 13:04:12 +0200

    Ecriture des tests de comparaison

--- src/comparateurs/ComparateurTotal.java
+++ src/comparateurs/ComparateurTotal.java
@@ -5,5 +5,11 @@ import java.util.Comparator;
 import participants.Pays;
 
 public class ComparateurTotal implements Comparator<Pays>{
+
+    private int sommeMedaille(Pays pays){
+        return pays.getMedailleArgent() + pays.getMedailleBronze() + pays.getMedailleOr();
+    }
+
+
     
 }
--- src/tests/TestComparateur.java
+++ src/tests/TestComparateur.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import org.junit.Test;
@@ -37,11 +38,101 @@ public class TestComparateur {
 
     @Test
     public void testCompTotal(){
-        
+        Pays france = new Pays("France");
+        Pays angleterre = new Pays("Angleterre");
+        Pays portugal = new Pays("Portugal");
+        Pays italie = new Pays("Italie");
+        Pays russie = new Pays("Russie");
+        Pays vatican = new Pays("Vatican");
+        Pays finlande = new Pays("Finlande");
+
+        List<Pays> lesPays = Arrays.asList(france,angleterre,portugal,italie,russie,vatican,finlande);
+        List<Pays> trie = Arrays.asList(vatican, angleterre,finlande,france,russie,italie,portugal);
+
+        // Ajout des médailles
+
+        france.addMedailleOr(69);
+        france.addMedailleArgent(42);
+        france.addMedailleBronze(404);
+
+        angleterre.addMedailleOr(15);
+        angleterre.addMedailleArgent(12);
+        angleterre.addMedailleBronze(666);
+
+        portugal.addMedailleOr(15);
+        portugal.addMedailleArgent(13);
+        portugal.addMedailleBronze(2);
+
+        italie.addMedailleOr(15);
+        italie.addMedailleArgent(12);
+        italie.addMedailleBronze(6);
+
+        russie.addMedailleOr(1);
+        russie.addMedailleArgent(55);
+        russie.addMedailleBronze(0);
+
+        vatican.addMedailleOr(666);
+        vatican.addMedailleArgent(666);
+        vatican.addMedailleBronze(666);
+
+        finlande.addMedailleOr(667);
+        finlande.addMedailleArgent(0);
+        finlande.addMedailleBronze(0);
+
+        Comparator<Pays> comp = new ComparateurTotal();
+
+        Collections.sort(lesPays,comp);
+
+        assertEquals(lesPays, trie);
     }
 
     @Test
     public void testCompMedailles(){
+        Pays france = new Pays("France");
+        Pays angleterre = new Pays("Angleterre");
+        Pays portugal = new Pays("Portugal");
+        Pays italie = new Pays("Italie");
+        Pays russie = new Pays("Russie");
+        Pays vatican = new Pays("Vatican");
+        Pays finlande = new Pays("Finlande");
+
+        List<Pays> lesPays = Arrays.asList(france,angleterre,portugal,italie,russie,vatican,finlande);
+        List<Pays> trie = Arrays.asList(vatican, france,portugal,angleterre,italie,russie,finlande);
+
+        // Ajout des médailles
+
+        france.addMedailleOr(69);
+        france.addMedailleArgent(42);
+        france.addMedailleBronze(404);
+
+        angleterre.addMedailleOr(15);
+        angleterre.addMedailleArgent(12);
+        angleterre.addMedailleBronze(666);
+
+        portugal.addMedailleOr(15);
+        portugal.addMedailleArgent(13);
+        portugal.addMedailleBronze(2);
+
+        italie.addMedailleOr(15);
+        italie.addMedailleArgent(12);
+        italie.addMedailleBronze(6);
+
+        russie.addMedailleOr(1);
+        russie.addMedailleArgent(55);
+        russie.addMedailleBronze(0);
+
+        vatican.addMedailleOr(666);
+        vatican.addMedailleArgent(666);
+        vatican.addMedailleBronze(666);
+
+        finlande.addMedailleOr(0);
+        finlande.addMedailleArgent(0);
+        finlande.addMedailleBronze(0);
+
+        Comparator<Pays> comp = new ComparateurMedailles();
+
+        Collections.sort(lesPays,comp);
         
+        assertEquals(lesPays, trie);
     }
 }
