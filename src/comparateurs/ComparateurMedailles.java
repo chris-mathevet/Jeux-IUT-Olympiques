612f51043ac8402e53678de34692626064094d32
Slyjack <62718253+slyjack999@users.noreply.github.com>
2024-06-05 10:57:41 +0200

    Créations des classes comparateurs (Code vide)

---
+++ src/comparateurs/ComparateurMedailles.java
@@ -0,0 +1,9 @@
+package comparateurs;
+
+import java.util.Comparator;
+
+import participants.Pays;
+
+public class ComparateurMedailles implements Comparator<Pays>{
+    
+}
---
+++ src/comparateurs/ComparateurTotal.java
@@ -0,0 +1,9 @@
+package comparateurs;
+
+import java.util.Comparator;
+
+import participants.Pays;
+
+public class ComparateurTotal implements Comparator<Pays>{
+    
+}
