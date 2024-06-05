b0e701f771ae6a12b0a9a1e194e3561279b9bac6
Slyjack <62718253+slyjack999@users.noreply.github.com>
2024-06-05 13:09:33 +0200

    ComparateurTotale

--- src/comparateurs/ComparateurTotal.java
+++ src/comparateurs/ComparateurTotal.java
@@ -6,10 +6,16 @@ import participants.Pays;
 
 public class ComparateurTotal implements Comparator<Pays>{
 
+    public ComparateurTotal(){
+        super();
+    }
+
     private int sommeMedaille(Pays pays){
         return pays.getMedailleArgent() + pays.getMedailleBronze() + pays.getMedailleOr();
     }
 
-
-    
+    @Override
+    public int compare(Pays p1, Pays p2) {
+        return this.sommeMedaille(p1)-this.sommeMedaille(p2);
+    }    
 }
