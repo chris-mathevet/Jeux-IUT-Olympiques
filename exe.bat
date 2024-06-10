6f1f2495f3f982ce62e38a6fbb925bda8bf3aa97
Slyjack <62718253+slyjack999@users.noreply.github.com>
2024-06-10 22:12:08 +0200

    Changement exec

--- exe.bat
+++ exe.bat
@@ -3,6 +3,7 @@ javac -d ./bin/ src/exceptions/*.java
 javac -cp ./bin/ -d ./bin/ src/participants/*.java src/sports/*.java src/epreuves/*.java
 javac -cp ./bin/ -d ./bin/ src/comparateurs/*.java
 javac -d ./bin/ -cp ./bin/ src/executable/*.java
+cls
 java -cp ./bin/ executable.Executable
 @REM javac -cp "./lib/junit-4.13.2.jar;./bin/" -d ./bin/ src/tests/*.java
 @REM java -ea -cp "./lib/hamcrest-2.2.jar;./lib/junit-4.13.2.jar;./bin" org.junit.runner.JUnitCore tests.TestEquipe
