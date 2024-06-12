a13e7c43df362e07bb4864ba39a0af3e0b0673e9
Axel Meunier <hacksel002@gmail.com>
2024-06-13 00:21:02 +0200

    TestExec

--- src/tests/TestExec.java
+++ src/tests/TestExec.java
@@ -14,6 +14,7 @@ import Executable.*;
 import epreuves.Epreuve;
 import epreuves.Match;
 import exceptions.AlreadyInException;
+import exceptions.DoesntExistException;
 import exceptions.NotSameCountryException;
 import exceptions.NotSameGenderException;
 import participants.Athlete;
@@ -31,10 +32,30 @@ public class TestExec {
         jo = new JO();
         jo.addPays(new Pays("France"));
         jo.addSport("VolleyBall");
+        jo.creerEquipe();
     }
 
     @Test
     public void testGetPays() {
-        assertEquals(jo.getPays("VolleyBall"), new Pays("VolleyBall"));
+
+        boolean testDoesnt = false;
+
+
+        try {
+            jo.getPays("France");
+        } catch (DoesntExistException e) {
+            System.err.println(e.getMessage());
+        }
+
+        try {
+            jo.getPays("Congo");
+        } catch (DoesntExistException e) {
+            System.err.println(e.getMessage());
+            testDoesnt = true;
+        }
+
+        assertTrue(testDoesnt);
+        
     }
+    
 }
