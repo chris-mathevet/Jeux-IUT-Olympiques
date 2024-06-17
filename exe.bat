a7de01702ed8e4b1a695c1f7044df7511ee6ec04
Slyjack <62718253+slyjack999@users.noreply.github.com>
2024-06-17 16:07:21 +0200

    Changement du bat

--- exe.bat
+++ exe.bat
@@ -1,18 +1,19 @@
 @REM @echo off
-cls
+
+set chemin="C:/localApps/openjfx/lib"
+echo %chemin%
+
 javac -d ./bin/ src/exceptions/*.java
 javac -cp ./bin/ -d ./bin/ src/participants/*.java src/sports/*.java src/epreuves/*.java
 javac -cp ./bin/ -d ./bin/ src/comparateurs/*.java
 javac -d ./bin/ -cp ./bin/ src/executable/*.java
-javac -d ./bin/ -cp ./bin/ --module-path C:\localApps\openjfx\lib --add-modules javafx.controls,javafx.fxml src/vues/*.java
+javac -d ./bin/ -cp ./bin/ --module-path %chemin% --add-modules javafx.controls,javafx.fxml src/MVC/modele/*.java 
+javac -d ./bin/ -cp ./bin/ --module-path %chemin% --add-modules javafx.controls,javafx.fxml src/MVC/vues/*.java src/MVC/controleur/*.java
+
 @REM cls
 @REM java -cp ./bin/ executable.Executable
-java -cp ./bin/ --module-path C:\localApps\openjfx\lib --add-modules javafx.controls,javafx.fxml vues.ApplicationJO
+java -cp ./bin/ --module-path %chemin% --add-modules javafx.controls,javafx.fxml MVC.vues.AppliJO
 @REM javac -cp "./lib/junit-4.13.2.jar;./bin/" -d ./bin/ src/tests/*.java
 @REM java -ea -cp "./lib/hamcrest-2.2.jar;./lib/junit-4.13.2.jar;./bin" org.junit.runner.JUnitCore tests.TestEquipe
 @REM java -ea -cp "./lib/hamcrest-2.2.jar;./lib/junit-4.13.2.jar;./bin" org.junit.runner.JUnitCore tests.TestEpreuveMatch
 @REM java -ea -cp "./lib/hamcrest-2.2.jar;./lib/junit-4.13.2.jar;./bin" org.junit.runner.JUnitCore tests.TestSport
-
-
-
-
