0870a1bdbc21140e11b4a11fddf427cbcfca3304
Slyjack <62718253+slyjack999@users.noreply.github.com>
2024-06-17 10:44:36 +0200

    Test css

---
+++ src/vues/ApplicationJO.java
@@ -0,0 +1,55 @@
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class ApplicationJO extends Application{
+
+    @Override
+    public void init() throws Exception {
+        
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        URL url = new File("FXML/Page_app.fxml").toURI().toURL();
+        FXMLLoader loader = new FXMLLoader(url);
+        BorderPane racine = loader.load();
+        Scene scene = new Scene(racine);
+        stage.setTitle("Puissance 4");
+        stage.setScene(scene);
+
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }   
+}
