475c23ee0120851698387fc6d6253fa1c4e0381d
TronnoxUwU <baptisterichard98@free.fr>
2024-06-18 15:10:47 +0200

    Test DB Athlete

--- src/database/exeTest.java
+++ src/database/exeTest.java
@@ -50,18 +50,41 @@ public class ExeTest {
                 System.out.println("==========================================");
                 System.out.println("tous les users");
                 r.selectAllUser();
-                // r.insertAthlete(toi);
-
-                // for (Athlete athlete : listeAthletes) {
-                //     System.out.println("truc1"+athlete);    
-                // }
-                // // r.insertAthlete(toi);
-
-
-            } catch (Exception e) {
+            } catch (SQLException e) {
+                System.err.println("USER");
                 System.err.println("erreur selse"+e);
             }
-            
+            try {
+                System.out.println("==========================================");
+
+                Pays france = new Pays("France");
+                Pays russie = new Pays("Russie");
+                Pays italie = new Pays("Italie");
+                r.insertPays(france);
+                r.insertPays(russie);
+                r.insertPays(italie);
+                
+                Athlete raphael  = new Athlete("Raphael", "Nadal", 'H', 9, 8, 5, france);
+                Athlete sophie  = new Athlete("Sophie", "Duke", 'F', 9, 8, 5, russie);
+                Athlete thomas  = new Athlete("Thomas", "King", 'F', 9, 8, 5, russie);
+                Athlete harry  = new Athlete("Harry", "Potter", 'H', 9, 8, 5, italie);
+    
+                r.insertAthlete(raphael);
+                r.insertAthlete(sophie);
+                r.insertAthlete(thomas);
+                r.insertAthlete(harry);
+                List<Athlete> res = r.selectAthlete();
+                for(Athlete a:res){
+                    System.out.println(a);
+                }
+                System.out.println(r.rechercherAthletes("russie",null,null));
+                System.out.println(r.rechercherAthletes(null,"Duke",null));
+                System.out.println(r.rechercherAthletes(null,null,"Raphael"));
+            }
+            catch(SQLException e){
+                System.err.println("Athlete");
+                System.err.println("erreur selse"+e);
+            }
         } catch (SQLException e) {
             System.err.println("euuuuuh pas trouvé, ca marche pas");
         }
