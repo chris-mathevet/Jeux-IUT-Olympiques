11f66ea24896e4c02eae259b4e7c9f19049dfeae
TronnoxUwU <baptisterichard98@free.fr>
2024-06-18 14:30:25 +0200

    test DB

--- exedb.bat
+++ exedb.bat
@@ -5,12 +5,10 @@ cls
 
 @REM jdbc seul (exeutable): version 5 (version du serveur car flemme de mettre a jour lol)
 
-set chemin="C:\Julian\lib_java\javafx-sdk-22\lib" 
-
 javac -d ./bin/ src/exceptions/*.java
 javac -cp ./bin/ -d ./bin/ src/participants/*.java src/sports/*.java src/epreuves/*.java
 javac -cp ./bin/ -d ./bin/ src/comparateurs/*.java
 javac -d ./bin/ -cp ./bin/ src/executable/*.java
-javac -d ./bin/ -cp "C:\Users\marqu\OneDrive\Bureau\SAEjava\SAE-JAVA\lib\mysql-connector-java-5.1.44.jar;./bin" src/database/*.java
+javac -d ./bin/ -cp ".\lib\mysql-connector-java-5.1.44.jar;./bin" src/database/*.java
 
-java -classpath "C:\Users\marqu\OneDrive\Bureau\SAEjava\SAE-JAVA\lib\mysql-connector-java-5.1.44.jar;./bin" database.ExeTest
+java -classpath ".\lib\mysql-connector-java-5.1.44.jar;./bin" database.ExeTest
