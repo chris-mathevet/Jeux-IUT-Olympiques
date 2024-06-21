1c79d52e68987cb7d9974dd7978bf8f13b9a2abe
LeskGit <hacksel002@gmail.com>
2024-06-21 10:43:21 +0200

    ExeLinux

--- exeAppli.sh
+++ exeAppli.sh
@@ -1,15 +1,12 @@
-clear
+#!/bin/bash
+
+# Compilation
 javac -d ./bin/ src/exceptions/*.java
-javac -cp ./bin/ -d ./bin/ src/participants/*.java src/sports/*.java src/epreuves/*.java
-javac -cp ./bin/ -d ./bin/ src/comparateurs/*.java
-javac -cp ./bin/ -d ./bin/ -cp ./bin/ src/executable/*.java
-javac -d ./bin/ -cp ".\lib\mysql-connector-java-5.1.44.jar:./bin" src/database/*.java src/MVC/modele/*.java 
-javac -cp ./bin/ -d ./bin/ --module-path /usr/share/openjfx/lib --add-modules javafx.controls,javafx.fxml src/MVC/tableClass/*.java
-javac -cp ./bin/ -d ./bin/ --module-path /usr/share/openjfx/lib --add-modules javafx.controls,javafx.fxml src/MVC/vues/*.java src/MVC/controleur/*.java
-javac -d ./bin/ -cp ./lib/mysql-connector-java-5.1.44.jar:./bin src/database/*.java
-java -cp ./bin/:./img/autre:./lib/mysql-connector-java-5.1.44.jar  --module-path /usr/share/openjfx/lib --add-modules javafx.controls,javafx.fxml MVC.vues.AppliJO
+javac -cp ./bin -d ./bin src/participants/*.java src/sports/*.java src/epreuves/*.java
+javac -cp ./bin -d ./bin src/comparateurs/*.java
+javac -cp ./bin -d ./bin src/executable/*.java
+javac -d ./bin -cp ./lib/mysql-connector-j-8.3.0.jar:./bin --module-path /usr/share/openjfx/lib --add-modules javafx.controls,javafx.fxml src/database/*.java src/MVC/modele/*.java src/MVC/user/*.java src/MVC/tableClass/*.java src/MVC/vues/*.java src/MVC/controleur/*.java
 
-
-
-
+# # Exécution
+java -cp ./bin:./img/autre:./img/pays:./img/sexe:./img/sports:./lib/mysql-connector-java-5.1.44.jar --module-path /usr/share/openjfx/lib --add-modules javafx.controls,javafx.fxml MVC.vues.AppliJO
 
